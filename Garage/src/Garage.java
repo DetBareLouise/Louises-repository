34b326fe28c2019868882fe22a6abb49a502619d
DetBareLouise <78900475+DetBareLouise@users.noreply.github.com>
2021-03-25 23:18:24 +0100

    Create Garage.java

---
+++ Garage/src/Garage.java
@@ -0,0 +1,10 @@
+public class Garage {
+
+    ArrayList<String> bilerIGaragen = new ArrayList<String>();
+     for (int i=0;i<bilerIGaragen.size();i++){
+
+        System.out.println(bilerIGaragen.get(i));
+
+
+        beregnGrønEjerAfgiftForBilpark();
+}
