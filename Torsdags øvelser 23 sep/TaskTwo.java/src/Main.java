507c6220837cdbcf9651018e06597f73a578590d
DetBareLouise <78900475+DetBareLouise@users.noreply.github.com>
2021-09-23 11:32:41 +0200

    TaskTwo

---
+++ Torsdags øvelser 23 sep/TaskTwo.java/src/Main.java
@@ -0,0 +1,34 @@
+class Main{
+    public static boolean happy = true;
+
+    public static void main(String [] args) {
+        if (iAmHappy())
+        {
+            System.out.println("I clap my hands");
+        }
+        else
+        {
+            System.out.println("I don't clap my hands");
+        }
+    }
+
+
+    public static boolean iAmHappy()
+    {
+        // fill out what is missing here:
+        if (iAmHappy()==true) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+}
+
+int y = 25;
+int x = 35;
+
+sum=x+y;
+
+System.out.println("the sum of the two numbers is", sum);
