facb9f2e9cb0f98f354c16514280a9dbed484722
DetBareLouise <78900475+DetBareLouise@users.noreply.github.com>
2021-09-23 14:38:10 +0200

    task 4

---
+++ Torsdags øvelser 23 sep/TaskFour/src/Main.java
@@ -0,0 +1,40 @@
+//task 4a
+public class newIntArray {
+
+    public static void main(int[] args) {
+        int[] numre = new int[6];
+        int[0] = 5;
+        int[1] = 10;
+        int[2] = 15;
+        int[3] = 20;
+        int[4] = 25;
+        int[5] = 30;
+        System.out.println(numre);
+
+    }
+}
+
+public class newStringArray<arr> {
+String[] arr = new String[3];
+
+    class String {
+    }
+        arr[0] = "Louise";
+        arr[1] = "er";
+        arr[2] = "fantastisk";
+        System.out.println(arr);
+    }
+}
+
+class booleanArray {
+
+}
+
+//4b
+class array {
+
+    public static void main(newStringArray.String[] args) {
+    newStringArray.String[] arr[]{"BMW", "Volvo", "Audi"};
+    }
+    System.out.println(newStringArray);
+}
