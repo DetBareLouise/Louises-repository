2aa61af924c8b37d78dafe20cc863332ff5da91a
DetBareLouise <78900475+DetBareLouise@users.noreply.github.com>
2021-09-23 14:58:03 +0200

    Task5

---
+++ Torsdags øvelser 23 sep/TaskFive/src/Datamatik.java
@@ -0,0 +1,13 @@
+//task 5a
+public class Datamatik {
+    Student datamatiker[] = new Student[10];
+    datamatiker[1] = new student("Zack");
+    datamatiker[2] = new student("Mia");
+    datamatiker[3] = new student("Malene");
+    datamatiker[4] = new student("Sofia");
+    datamatiker[5] = new student("Casper");
+    datamatiker[6] = new student("Morten");
+    datamatiker[7] = new student("Kim");
+    datamatiker[8] = new student("Muneeb");
+    datamatiker[9] = new student("Betul");
+}
---
+++ Torsdags øvelser 23 sep/TaskFive/src/Student.java
@@ -0,0 +1,15 @@
+public class Student{
+
+    String name;
+    int age;
+    boolean isFemale;
+    boolean datamatikerTeam;
+
+
+    public void student(stringName, intAge, booleanisFemale, booleandatamatikerTeam) {
+        this.name = name;
+        this.age = age;
+        this.isFemale = isFemale;
+        this.datamatikerTeam = datamatikerTeam;
+    }
+}
