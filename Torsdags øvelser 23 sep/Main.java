8c36913e49f08f2ea3bf056508b1737a01c79c35
DetBareLouise <78900475+DetBareLouise@users.noreply.github.com>
2021-09-23 11:00:55 +0200

    Create Main.java

---
+++ Torsdags øvelser 23 sep/Main.java
@@ -0,0 +1,22 @@
+// task 1 functions
+
+public class Main{
+
+public static void main(String[] args) {
+
+  getName("Louise");
+
+}
+
+public static void getName(String navn){
+  System.out.println();
+
+}
+
+public int getAge(){
+  int age = 24;
+  return age;
+
+}
+
+}
